b6fc817d58b2a5e54a71c1614e43cf046f597012
cory-tetraprime <cory@tetraprimeconsulting.com>
2024-08-24 10:20:40 -0400

    Latest code.

--- home/models.py
+++ home/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 
+from django.apps import apps
 from wagtail.models import Page
 from wagtail.fields import RichTextField
 from wagtail.admin.panels import FieldPanel
+# from blog.models import BlogPage
 
 
 class HomePage(Page):
@@ -11,3 +13,10 @@ class HomePage(Page):
     content_panels = Page.content_panels + [
         FieldPanel('body'),
     ]
+
+    def get_context(self, request):
+        BlogPage = apps.get_model('blog', 'BlogPage')
+        context = super().get_context(request)
+        featured_posts = BlogPage.objects.live().filter(home_featured=True).order_by('-date')
+        context['featured_posts'] = featured_posts
+        return context
